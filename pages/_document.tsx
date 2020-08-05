82d7df9d3abfb9a6b74c97487ae046851beecc51
tobb422 <tobb422@gmail.com>
2020-08-05 09:14:26 +0900

    default style

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ export default class MyDocument extends Document {
       <html>
       <Head>
         <link rel="shortcut icon" type="image/x-icon" href="/static/favicon.ico" />
-        <style>{`body { margin: 0 } /* custom!*/`}</style>
+        <link rel="stylesheet" type="text/css" href="/static/main.css" />
       </Head>
       <body className="custom_class">
       <Main />
